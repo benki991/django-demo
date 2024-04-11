61725c90057f090da157c898c35bbc430410b7b2
Kevin Payos <kevinbarret.payos@gmail.com>
2024-04-11 10:04:25 +0800

    Added login db

--- testProject/testApp/admin.py
+++ testProject/testApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, user_login
 # Register your models here.
 admin.site.register(TestModel)
+admin.site.register(user_login)
--- testProject/testApp/models.py
+++ testProject/testApp/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class TestModel(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+
+class user_login(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
